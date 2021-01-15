350df7597f987f5c92723f91eb4c2cab97911875
Laila Reed <lreed606@noscihigh.org>
2021-01-15 20:35:46 +0000

    finished

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ Declare another variable named lastName and assign it the value of your last nam
 Then declare another variable named fullName and assign it to the current value of firstName and lastName and have the joined values separated with a space character.
 Remember that computer languages will not add the space character for you so how would you do that manually?
 */
+var firstName = laila
+var lastName = reed
 
-
+var fullName = laila reed
 
 
 //Numbers
@@ -20,10 +22,13 @@ Declare a variable named luckyNumber.
 Assign luckyNumber to a number of your choice.
 Declare another variable named luckyNumberTimes2 and assign it the value of luckyNumber multiplied by 2.
 */
-
+var luckyNumber = 28
+var luckyNumberTimes2 = 56
 
 //Booleans
 /*
 Declare a variable named myBool and assign it the value of true.
 Then, declare another named isEqualTo and assign it the value of strictly comparing luckyNumber and luckyNumberTimes2.
 */
+var myBool = true
+var isEqualTo = 56 + 28
